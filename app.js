0632ac6ffb1beb41e999f056df48b811ff796dbb
BoDee Angus <bodee.angus@gmail.com>
2024-03-06 14:26:47 -0700

    Finished Package

---
+++ app.js
@@ -0,0 +1,15 @@
+import inquirer from 'inquirer';
+
+export default async function (question) {
+  try {
+    const { [question]: response } = await inquirer.prompt([
+      { name: question },
+    ]);
+    console.log('Terminal_Asker successfully received a response:');
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.error('Terminal_Asker encountered an error:');
+    console.error(error);
+  }
+}
---
+++ appTester.js
@@ -0,0 +1,3 @@
+import app from './app.js';
+
+app('Is this is a test question?');
